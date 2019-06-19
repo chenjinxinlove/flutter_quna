4f8d3165c00460401cd136c712d6f416a769034f
chenjinxin <chenjinxin@zhuanzhuan.com>
2019-06-19 19:45:44 +0800

    首页model

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_quna/dao/home_dao.dart';
+import 'package:flutter_quna/model/home_model.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+const APPBAR_SCROLL_OFFSET = 100;
+
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,14 +14,98 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List _imageUrls = [
+    'https://imgsa.baidu.com/news/q%3D100/sign=ebefe45e5782b2b7a19f3dc401accb0a/8cb1cb134954092397ff1f249c58d109b2de49d1.jpg',
+    'https://imgsa.baidu.com/news/q%3D100/sign=fefabe4d6cd0f703e0b291dc38fb5148/3bf33a87e950352ab2b5cd525d43fbf2b3118b90.jpg',
+    'https://imgsa.baidu.com/news/q%3D100/sign=e7057f8606fa513d57aa68de0d6c554c/c75c10385343fbf2c49b4c79be7eca8065388f31.jpg'
+  ];
+  double appBarAlpha = 0;
+  String resultString;
+  _onScroll(offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if(alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    try {
+      HomeModel model = await HomeDao.fetch();
+      setState(() {
+        resultString = json.encode(model);
+      });
+    } catch (e) {
+      setState(() {
+        resultString = e.toString();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        body: Center(
-          child: Text('首页'),
-        )
+      body: Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification && scrollNotification.depth == 0) {
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _imageUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination: SwiperPagination(),
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(title: Text(resultString),),
+                  )
+                ],
+              ),
+            )
+          ),
+          Opacity(
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(padding: EdgeInsets.only(top: 20), 
+                  child: Text('首页'),
+                ),
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
-
 }
